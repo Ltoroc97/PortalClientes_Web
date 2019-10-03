4718930eaf999a6a9486347bc9dead880db20e92
Lorena Toro <Lltoro2@misena.edu.co>
2019-10-03 09:23:46 -0500

    Se agrega nueva funcionalidad

---
+++ Object Repository/Mis Afiliados/div_Victoria Eugenia Vargas De Navarro              Cc 21371502.rs
@@ -0,0 +1,67 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Victoria Eugenia Vargas De Navarro              Cc 21371502</name>
+   <tag></tag>
+   <elementGuidId>ecf680a2-edcc-4622-890b-72d16f50002e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='conten2']/div/div/div/div/div</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>emi-color-cards-ben</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+              Victoria Eugenia Vargas De Navarro
+              Cc 21371502
+            </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/app-admin[1]/section[2]/div[@id=&quot;conten2&quot;]/div[@class=&quot;container-modules-menu-top&quot;]/app-my-beneficiaries[1]/section[@class=&quot;container-my-affiliates&quot;]/div[@id=&quot;conten2&quot;]/div[1]/div[@class=&quot;col-md-12&quot;]/div[1]/div[@class=&quot;col-md-6 col-sm-12 col-xs-12&quot;]/div[@class=&quot;emi-color-cards-ben&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='conten2']/div/div/div/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Inicio / Mi cuenta / Mis beneficiarios'])[1]/following::div[6]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/div/div/div/div/div</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/FlujoCompleto_Modulo3_ConsultarMisAfiliados/ConsultaMisAfiliados.ts
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ConsultaMisAfiliados</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>9380e793-a785-48b7-bbe5-73035b009e28</testSuiteGuid>
+   <testCaseLink>
+      <guid>d68319a3-179c-4785-9452-d3d8cb88cee6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AbrirNavegador/AbrirNavegador</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>047fee2a-0677-4898-95c1-67ba5715bf2d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IniciarSesion/IngresarInformacion</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5012b46c-c63d-49e0-bd0d-c6bb3daeb2b9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/IniciarSesion/SeleccionarBoton_Ingresar</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8438ad82-5ca6-495e-a18a-6018b9326640</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mi Cuenta/Consultar Afiliados/SeleccionarMenu_MiCuenta</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d6da1e92-1a66-4437-8509-432b3c657ced</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mi Cuenta/Consultar Afiliados/SeleccionarOpcion_MisAfiliados</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e83e0bc1-ed7c-44b9-af52-d0f39fa88537</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Mi Cuenta/Consultar Afiliados/VisualizarInformacion_MisAfiliados</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
